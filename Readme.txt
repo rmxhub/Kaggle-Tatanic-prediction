58c7fb57d8543b7dcbdf6b891f912bcc0ab3e4de
rmxhub <xia7820@gmail.com>
2015-08-08 20:41:20 -0500

    final

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+Compared eight algorithm, it seems that Random Forest has the best prediction result
+
+['Nearest Neighbors is:0.70']
+['Linear SVM is:0.79']
+['RBF SVM is:0.64']
+['Decision Tree is:0.81']
+['Random Forest is:0.82']
+['AdaBoost is:0.81']
+['Naive Bayes is:0.79']
+['LDA is:0.79']
+['QDA is:0.80']
---
+++ Titanic code.py
@@ -0,0 +1,154 @@
+
+import os
+import pandas as pd
+import numpy as np
+import csv as csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import cross_validation
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.lda import LDA
+from sklearn.qda import QDA
+
+import scipy as sp
+def llfun(act, pred):
+    epsilon = 1e-15
+    pred = sp.maximum(epsilon, pred)
+    pred = sp.minimum(1-epsilon, pred)
+    ll = sum(act*sp.log(pred) + sp.subtract(1,act)*sp.log(sp.subtract(1,pred)))
+    ll = ll * -1.0/len(act)
+    return ll
+
+os.chdir("E:\Kaggle\Titanic")
+
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree",
+         "Random Forest", "AdaBoost", "Naive Bayes", "LDA", "QDA"]
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=100, max_features=3),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    LDA(),
+    QDA()]
+
+
+# Data cleanup
+# TRAIN DATA
+train_df = pd.read_csv('train.csv', header=0)        # Load the train file into a dataframe
+list(train_df.columns.values)
+
+# I need to convert all strings to integer classifiers.
+# I need to fill in the missing values of the data and make it complete.
+
+# female = 0, Male = 1
+train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+# Embarked from 'C', 'Q', 'S'
+# Note this is not ideal: in translating categories to numbers, Port "2" is not 2 times greater than Port "1", etc.
+
+# All missing Embarked -> just make them embark from most common place
+if len(train_df.Embarked[ train_df.Embarked.isnull() ]) > 0:
+    train_df.Embarked[ train_df.Embarked.isnull() ] = train_df.Embarked.dropna().mode().values
+
+Ports = list(enumerate(np.unique(train_df['Embarked'])))    # determine all values of Embarked,
+Ports_dict = { name : i for i, name in Ports }              # set up a dictionary in the form  Ports : index
+train_df.Embarked = train_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
+
+# All the ages with no data -> make the median of all Ages
+median_age = train_df['Age'].dropna().median()
+if len(train_df.Age[ train_df.Age.isnull() ]) > 0:
+    train_df.loc[ (train_df.Age.isnull()), 'Age'] = median_age
+
+# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
+train_df = train_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+
+
+# TEST DATA
+test_df = pd.read_csv('test.csv', header=0)        # Load the test file into a dataframe
+
+# I need to do the same with the test data now, so that the columns are the same as the training data
+# I need to convert all strings to integer classifiers:
+# female = 0, Male = 1
+test_df['Gender'] = test_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+# Embarked from 'C', 'Q', 'S'
+# All missing Embarked -> just make them embark from most common place
+if len(test_df.Embarked[ test_df.Embarked.isnull() ]) > 0:
+    test_df.Embarked[ test_df.Embarked.isnull() ] = test_df.Embarked.dropna().mode().values
+# Again convert all Embarked strings to int
+test_df.Embarked = test_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+
+
+# All the ages with no data -> make the median of all Ages
+median_age = test_df['Age'].dropna().median()
+if len(test_df.Age[ test_df.Age.isnull() ]) > 0:
+    test_df.loc[ (test_df.Age.isnull()), 'Age'] = median_age
+
+# All the missing Fares -> assume median of their respective class
+if len(test_df.Fare[ test_df.Fare.isnull() ]) > 0:
+    median_fare = np.zeros(3)
+    for f in range(0,3):                                              # loop 0 to 2
+        median_fare[f] = test_df[ test_df.Pclass == f+1 ]['Fare'].dropna().median()
+    for f in range(0,3):                                              # loop 0 to 2
+        test_df.loc[ (test_df.Fare.isnull()) & (test_df.Pclass == f+1 ), 'Fare'] = median_fare[f]
+
+# Collect the test data's PassengerIds before dropping it
+ids = test_df['PassengerId'].values
+# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
+test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+
+
+# The data is now ready to go. So lets fit to the train, then predict to the test!
+# Convert back to a numpy array
+train_data = train_df.values
+test_data = test_df.values
+
+
+print 'Training...'
+#RandomForest with cross-validation
+forest = RandomForestClassifier(max_depth=5, n_estimators=100, max_features=3)
+#Simple K-Fold cross validation. 5 folds.
+cv = cross_validation.KFold(len(train_data), n_folds=4)  
+results = []
+for traincv, testcv in cv:
+    probas = forest.fit(train_data[traincv,1::], train_data[traincv,0]).predict_proba(train_data[testcv,1::])
+    results.append( llfun(train_data[testcv,0], [x[1] for x in probas]) )
+#print out the mean of the cross-validated results
+print "Results: " + str( np.array(results).mean() )
+
+
+
+#many classifiers comparison
+results = []
+score=[]
+for name, clf in zip(names, classifiers):
+    results = []
+    score=[]
+    for traincv, testcv in cv:
+        probas = clf.fit(train_data[traincv,1::], train_data[traincv,0])
+        score.append(clf.score(train_data[testcv,1::], train_data[testcv,0]))
+    scoremean=np.array(score).mean()
+    results.append( name+' is:'+str(('%.2f' % scoremean).lstrip('0')))
+    #print out the mean of the cross-validated results
+    print str(results)
+
+#RandomForest test results
+forest = forest.fit( train_data[0::,1::], train_data[0::,0] )
+
+print 'Predicting...'
+output = forest.predict(test_data).astype(int)
+
+
+predictions_file = open("TitanicRandonForest.csv", "wb")
+open_file_object = csv.writer(predictions_file)
+open_file_object.writerow(["PassengerId","Survived"])
+open_file_object.writerows(zip(ids, output))
+predictions_file.close()
+print 'Done.'
